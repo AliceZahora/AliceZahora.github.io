f62c58fb5607c33d809093013cadbd05b3579fc0
AliceZahora <zahora.alice@gmail.com>
2026-09-02 20:57:03 -0500

    Explained differences in variable types via js comment

--- web200/assignment2/assignment2.js
+++ web200/assignment2/assignment2.js
@@ -1,6 +1,22 @@
+/*
+Alice Zahora
+Class: Web 200
+Assignment: 2
+Date: 09/05/2026
+
+Var vs Let vs Const:
+    While all of these declarations are used to add variables to code,
+    their slight differences make them best for different applications.
+    Unlike the other two, var can be redeclared. It is also more global,
+    accessible outside functions and brackets where it was originally declared.
+    This cannot be said for let or const, which can only be used within its
+    origin block. While it can be reassigned, it cannot be redeclared.
+    Const, on the other hand, must have one static value and cannot be reassigned.
+*/
+
 let binaryNum = 10;
-let joke = "There are " + binaryNum + " types of people in the world. Those who understand binary" +
-           ", and those who don't";
+let joke = "There are " + binaryNum + " types of people in the world." +
+            "Those who understand binary and those who don't";
 const pi = 3.14159;
 
 document.getElementById("binaryJoke").innerHTML = joke;
