f12b6136f7fc19b725982d27dbd06e339df8f504
AliceZahora <zahora.alice@gmail.com>
2026-09-04 22:35:54 -0500

    Styled joke spacing by changing html layout

--- web200/assignment2/assignment2.js
+++ web200/assignment2/assignment2.js
@@ -6,7 +6,7 @@ Date: 09/05/2026
 
 Var vs Let vs Const:
     While all of these are used to declare variables, their slight
-    differences make them best for different applications. Unlike 
+    differences make them best for different applications. Unlike
     the other two, 'var' can be redeclared. It is also more global,
     accessible outside functions and brackets where it was originally declared.
     This cannot be said for 'let' or 'const', which can only be used within their
@@ -15,8 +15,9 @@ Var vs Let vs Const:
 */
 
 let binaryNum = 10;
-let joke = "There are " + binaryNum + " types of people in the world." +
-            "Those who understand binary and those who don't";
+let joke = "<p>There are <br><p id='binaryNum'>" + binaryNum +
+            "</p><br>types of people in the world..." +
+            "<br>Those who understand binary and those who don't</p>";
 const pi = 3.14159;
 
 document.getElementById("binaryJoke").innerHTML = joke;
