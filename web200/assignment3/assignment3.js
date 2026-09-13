b2fbe741665ed4c0fda6af48459c10008129e57c
AliceZahora <zahora.alice@gmail.com>
2026-09-13 02:06:32 -0500

    Fixed unit type detection

--- web200/assignment3/assignment3.js
+++ web200/assignment3/assignment3.js
@@ -10,15 +10,18 @@ function tempConvert()
     let tempHTML = document.getElementById("tempNum");
     let currTemp = tempHTML.innerHTML;
     let tempTypeHTML = document.getElementById("tempType");
+    let tempType = tempTypeHTML.innerHTML;
     let newTemp;
 
-    if(tempType == "&degF")
+    if(tempType == " °F ")
     {
         newTemp = (currTemp - 32) * (5/9);
+        tempTypeHTML.innerHTML = " °C ";
     }
     else
     {
         newTemp = ((9/5) * currTemp) + 32;
+        tempTypeHTML.innerHTML = " °F ";
     }
 
     tempHTML.innerHTML = newTemp;
@@ -27,16 +30,19 @@ function speedConvert()
 {
     let speedHTML = document.getElementById("speedNum");
     let currSpeed = speedHTML.innerHTML;
-    let speedType = document.getElementById("speedType").innerHTML;
+    let speedTypeHTML = document.getElementById("speedType");
+    let speedType = speedTypeHTML.innerHTML;
     let newSpeed;
 
     if(speedType == "mph")
     {
         newSpeed = currSpeed * 1.6;
+        speedTypeHTML.innerHTML = "kmph";
     }
     else
     {
         newSpeed = currSpeed/1.6;
+        speedTypeHTML.innerHTML = "mph";
     }
 
     speedHTML.innerHTML = newSpeed;
