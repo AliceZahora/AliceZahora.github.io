ff614685ec79eff88a0bbee1866099715e85a4bf
AliceZahora <zahora.alice@gmail.com>
2026-09-13 01:37:40 -0500

    Made adjuster areas more compact

---
+++ web200/assignment3/assignment3.js
@@ -0,0 +1,9 @@
+/*
+Alice Zahora
+Class: Web 200
+Assignment: 3
+Date: 09/13/2026
+*/
+
+let currTemp = document.getElementById("temp").innerHTML;
+let currSpeed = document.getElementById("speed").innerHTML;
