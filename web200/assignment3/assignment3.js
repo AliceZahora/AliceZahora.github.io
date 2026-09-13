c3955985f88ef79f43bee64d4eaabedafeb0661a
AliceZahora <zahora.alice@gmail.com>
2026-09-13 01:57:40 -0500

    Added unit conversion logic + button onclick event

--- web200/assignment3/assignment3.js
+++ web200/assignment3/assignment3.js
@@ -5,14 +5,41 @@ Assignment: 3
 Date: 09/13/2026
 */
 
-let currTemp = document.getElementById("tempNum").innerHTML;
-let tempType = document.getElementById("tempType").innerHTML;
-let currSpeed = document.getElementById("speedNum").innerHTML;
-let speedType = document.getElementById("speedType").innerHTML;
-
 function tempConvert()
 {
+    let tempHTML = document.getElementById("tempNum");
+    let currTemp = tempHTML.innerHTML;
+    let tempTypeHTML = document.getElementById("tempType");
+    let newTemp;
 
+    if(tempType == "&degF")
+    {
+        newTemp = (currTemp - 32) * (5/9);
+    }
+    else
+    {
+        newTemp = ((9/5) * currTemp) + 32;
+    }
+
+    tempHTML.innerHTML = newTemp;
+}
+function speedConvert()
+{
+    let speedHTML = document.getElementById("speedNum");
+    let currSpeed = speedHTML.innerHTML;
+    let speedType = document.getElementById("speedType").innerHTML;
+    let newSpeed;
+
+    if(speedType == "mph")
+    {
+        newSpeed = currSpeed * 1.6;
+    }
+    else
+    {
+        newSpeed = currSpeed/1.6;
+    }
+
+    speedHTML.innerHTML = newSpeed;
 }
 
 
