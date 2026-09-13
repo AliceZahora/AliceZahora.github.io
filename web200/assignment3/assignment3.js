95ebaff922f270197c23370aa801aaca232ab95f
AliceZahora <zahora.alice@gmail.com>
2026-09-13 01:44:11 -0500

    Removed cloud, added units

--- web200/assignment3/assignment3.js
+++ web200/assignment3/assignment3.js
@@ -5,5 +5,14 @@ Assignment: 3
 Date: 09/13/2026
 */
 
-let currTemp = document.getElementById("temp").innerHTML;
-let currSpeed = document.getElementById("speed").innerHTML;
+let currTemp = document.getElementById("tempNum").innerHTML;
+let tempType = document.getElementById("tempType").innerHTML;
+let currSpeed = document.getElementById("speedNum").innerHTML;
+let speedType = document.getElementById("speedType").innerHTML;
+
+function tempConvert()
+{
+
+}
+
+
