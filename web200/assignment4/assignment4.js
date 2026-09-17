7d670e22278367b7e3ff3afb882dddc2d6ed9a11
AliceZahora <zahora.alice@gmail.com>
2026-09-16 20:48:28 -0500

    Set up calc elemenets and visuals

---
+++ web200/assignment4/assignment4.js
@@ -0,0 +1,12 @@
+function addNums(num1, num2)
+{
+    let sum = num1 + num2;
+}
+const multiplyNums = function(num1, num2)
+{
+    let product = num1 * num2;
+}
+const subtractNums = (num1, num2) =>
+{
+    let difference = num1 - num2;
+}
